2a94fac14103b95e6f9d21a7046983ed1b51c7c2
IHillson <hilaryaugustine8@gmail.com>
2026-08-12 16:49:07 +0100

    Update index.js

--- Real Estate/index.js
+++ Real Estate/index.js
@@ -1,25 +1,45 @@
 import { propertyForSaleArr } from "./properties/propertyForSaleArr.js"
 import { propertyPlaceholderObj } from "./properties/placeholderProperty.js"
 
-function createCardHTML({ propertyLocation, priceGBP, comment, image, roomsM2 }) {
-    const totalSizeM2 = roomsM2.reduce((total, roomSize) => total + roomSize, 0);
+// function createCardHTML({ propertyLocation, priceGBP, comment, image, roomsM2 }) {
+//     const totalSizeM2 = roomsM2.reduce((total, roomSize) => total + roomSize, 0);
 
-    return `
+//     return `
+//         <section class="card">
+//             <img src="images/${image}">
+//             <div class="card-right">
+//                 <h2>${propertyLocation}</h2>
+//                 <h3>£${priceGBP.toLocaleString()}</h3>
+//                 <p>${comment}</p>
+//                 <h3>${totalSizeM2} m&sup2;</h3>
+//             </div>
+//         </section>
+//     `
+// }
+
+// function getPropertyHtml(propertiesArr = [propertyPlaceholderObj]) {
+//     return propertiesArr.map(createCardHTML).join('')
+// }
+
+
+// document.getElementById('container').innerHTML = getPropertyHtml(propertyForSaleArr)
+
+// ALTERNATIVELY
+function getPropertyHtml(propertiesArr = [propertyPlaceholderObj]) {
+    return propertiesArr.map(property => {
+        const { propertyLocation, priceGBP, roomsM2, comment, image } = property
+        const totalRoomSizeM2 = roomsM2.reduce((total, current) => total + current)
+        return `
         <section class="card">
             <img src="images/${image}">
             <div class="card-right">
                 <h2>${propertyLocation}</h2>
                 <h3>£${priceGBP.toLocaleString()}</h3>
                 <p>${comment}</p>
-                <h3>${totalSizeM2} m&sup2;</h3>
+                <h3>${totalRoomSizeM2} m&sup2;</h3>
             </div>
-        </section>
-    `
+        </section> `
+    })
 }
-
-function getPropertyHtml(propertiesArr = [propertyPlaceholderObj]) {
-    return propertiesArr.map(createCardHTML).join('')
-}
-
 
 document.getElementById('container').innerHTML = getPropertyHtml(propertyForSaleArr)
