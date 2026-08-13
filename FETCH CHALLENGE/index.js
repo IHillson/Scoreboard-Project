720730c2626ecc0cc505ccc7c6b45b8905242401
IHillson <hilaryaugustine8@gmail.com>
2026-08-13 15:34:32 +0100

    Commit

---
+++ FETCH CHALLENGE/index.js
@@ -0,0 +1,37 @@
+// async function fetchUserActivity() {
+//     try {
+//         const response = await fetch('https://apis.scrimba.com/bored/api/activity')
+        
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! Status: ${response.status}`)
+//         }
+       
+//         const data = await response.json()
+//         console.log(data)
+//         return data
+//     } catch (error) {
+//         console.error('Fetch operation failed', error)
+//     }
+// }
+
+// fetchUserActivity()
+
+// async function getSuggestion() {
+//     const response = fetch('https://apis.scrimba.com/bored/api/activity')
+//     const data = await response.json()
+//     console.log(data)
+// }
+// getSuggestion()
+
+// async function getRandomDog() {
+//     try {
+//         const response = await fetch('https://apis.scrimba.com/bored/api/activity')
+//         const data = await response.json()
+//         console.log(data)
+//         return data
+//     } catch (err) {
+//         throw new Error('Fetch operation failed!', err)
+//     } finally {
+//         console.log('The operation completed.')
+//     }
+// }
