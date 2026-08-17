57a2a5e4aace553bbb11ea85535f582ba39ff4f2
IHillson <hilaryaugustine8@gmail.com>
2026-08-17 13:29:55 +0100

    Commit

--- FETCH CHALLENGE/index.js
+++ FETCH CHALLENGE/index.js
@@ -1,20 +1,22 @@
-// async function fetchUserActivity() {
-//     try {
-//         const response = await fetch('https://apis.scrimba.com/bored/api/activity')
+async function fetchUserActivity() {
+    try {
+        const response = await fetch('https://apis.scrimba.com/bored/api/activity')
         
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! Status: ${response.status}`)
-//         }
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`)
+        }
        
-//         const data = await response.json()
-//         console.log(data)
-//         return data
-//     } catch (error) {
-//         console.error('Fetch operation failed', error)
-//     }
-// }
+        const data = await response.json()
+        console.log(data)
+        return data
+    } catch (error) {
+        console.error('Fetch operation failed', error)
+    } finally {
+        console.log('The operation completed.')
+    }
+}
 
-// fetchUserActivity()
+fetchUserActivity()
 
 // async function getSuggestion() {
 //     const response = fetch('https://apis.scrimba.com/bored/api/activity')
--- FETCH/index.js
+++ FETCH/index.js
@@ -6,4 +6,69 @@ fetch('https://apis.scrimba.com/dog.ceo/api/breeds/image/random')
         imageElement.alt = 'random dog picture'
         document.getElementById("img-container").appendChild(imageElement)
     })
-    
+
+    // // POST REQUEST
+    // try {
+    //     const response = await fetch('https://apis.scrimba.com/placeholder/post',
+    //         {
+    //             method: 'POST',
+    //             body: JSON.stringify({
+    //                 title: 'Holiday Nigthmares',
+    //                 body: 'When I was kidnapped in Scotland..',
+    //                 userId: 101
+    //             }),
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             }
+    //         })
+    //     if (!response.ok) {
+    //         throw new Error('There is a problem with the API')
+    //     }
+    //     const data = await response.json()
+    //     console.log(data)
+    // } catch (err) {
+    //     console.log(err) 
+    // }
+
+    // // PUT REQUEST
+    // try {
+    //     const response = await fetch('https://apis.scrimba.com/placeholder/posts/1', {
+    //         method: 'PUT',
+    //         body: JSON.stringify({
+    //             id: 1,
+    //             title: 'Updated Post Title',
+    //             body: 'Updated post content.',
+    //             userId: 101
+    //         }),
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         }
+    //     })
+    //     if (!response.ok) {
+    //         throw new Error('There is a problem with the API')
+    //     }
+    //     const data = await response.json()
+    //     console.log(data)
+    // } catch (err) {
+    //     console.log(err)
+    // }
+
+    // // PATCH REQUEST
+    // try {
+    //     const response = await fetch('https://apis.scrimba.com/placeholder/posts/1', {
+    //         method: 'PATCH',
+    //         body: JSON.stringify({
+    //             title: 'Updated Post Title',
+    //         }),
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         }
+    //     })
+    //     if (!response.ok) {
+    //         throw new Error('There is a problem with the API')
+    //     }
+    //     const data = await response.json()
+    //     console.log(data)
+    // } catch (err) {
+    //     console.log(err)
+    // }
