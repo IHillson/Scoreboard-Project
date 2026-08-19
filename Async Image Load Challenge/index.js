2bf1ef1a901e564665e2931f8b2febae499ffd1e
IHillson <hilaryaugustine8@gmail.com>
2026-08-19 12:59:07 +0100

    commit

---
+++ Async Image Load Challenge/index.js
@@ -0,0 +1,36 @@
+function getImagePromise(url) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const img = new Image()
+            img.src = url
+            img.alt = 'Scenic Image'
+            img.addEventListener('load', () => resolve(img))
+            img.addEventListener('error', () => reject(new Error(`Failed to load image: ${url}`)))
+        }, 500)    
+    })
+}
+
+const images = [
+    'https://scrimba.com/links/advancedjs-resources-images-scenic1',
+    'https://scrimba.com/links/advancedjs-resources-images-scenic2',
+    'https://scrimba.com/links/advancedjs-resources-images-scenic3'
+]
+
+async function preLoadImages(imageUrlsArr) {
+    const imgContainer = document.getElementById('img-container')
+    const uploadContainer = document.getElementById('upload-container')
+    
+    const promises = imageUrlsArr.map((url) => getImagePromise(url))
+    
+    try {
+        const results = await Promise.all(promises)
+        console.log('All images loaded successfully!')
+        uploadContainer.style.display = 'none'
+        results.forEach((img) => imgContainer.appendChild(img))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+document.getElementById('submit-imgs').addEventListener('click', () => preLoadImages(images))
